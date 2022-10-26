b72c0bb5eeed0f4e034e81198106f7efa3834ae8
Haikal Fikri <fhaikal580@gmail.com>
2022-10-26 22:00:01 +0700

    tambah components

---
+++ src/components/SavedShows.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { UserAuth } from '../context/AuthContext';
+import { db } from '../firebase';
+import { updateDoc, doc, onSnapshot } from 'firebase/firestore';
+
+
+const SavedShows = () => {
+  const [movies, setMovies] = useState([]);
+  const { user } = UserAuth();
+
+  const slideLeft = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  
+
+  const movieRef = doc(db, 'users', `${user?.email}`)
+  const deleteShow = async (passedID) => {
+      try {
+        const result = movies.filter((item) => item.id !== passedID)
+        await updateDoc(movieRef, {
+            savedShows: result
+        })
+      } catch (error) {
+          console.log(error)
+      }
+  }
+
+  return (
+    <>
+      <h2 className='text-white font-bold md:text-xl p-4'>My Shows</h2>
+      <div className='relative flex items-center group'>
+        
+      </div>
+    </>
+  );
+};
+
+export default SavedShows;
